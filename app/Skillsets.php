fb8f689070c4c53f9f7c9b9b723ca1c557f94799
Karl T. Macadangdang <karljarren0308@gmail.com>
2019-10-30 15:00:00 +0800

    Added Skillsets:Skills relationship

--- app/Skillsets.php
+++ app/Skillsets.php
@@ -8,6 +8,11 @@ class Skillsets extends Model
 {
     public function user()
     {
-      return $this->belongsTo('App\Users', 'user_id', 'id');
+        return $this->belongsTo('App\Users', 'user_id', 'id');
+    }
+
+    public function skill()
+    {
+        return $this->belongsTo('App\Skills', 'skill_id', 'id');
     }
 }

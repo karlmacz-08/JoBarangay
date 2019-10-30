bf4d3ddce470df1514ee25d8bd47735884de4417
Karl T. Macadangdang <karljarren0308@gmail.com>
2019-10-30 15:02:11 +0800

    Added Users Modal

---
+++ app/Users.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class Users extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'mobile_number',
+        'password',
+        'first_name',
+        'middle_name',
+        'last_name',
+        'birth_date',
+        'sex',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token', 'confirmation',
+    ];
+
+    public function full_name()
+    {
+        if($this->middle_name !== null && $this->middle_name !== '') {
+            return $this->first_name . ' ' . substr($this->middle_name, 0, 1) . '. ' . $this->last_name;
+        } else {
+            return $this->first_name . ' ' . $this->last_name;
+        }
+    }
+
+    public function skills()
+    {
+      return $this->hasMany('App\Skillsets', 'user_id', 'id');
+    }
+}

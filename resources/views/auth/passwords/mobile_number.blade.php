3d6db8124243ebef0f78743854572ffb0d451da6
McPerry11 <mack.perry.co@gmail.com>
2019-10-30 15:07:41 +0800

    Login & Registratiob Update

--- resources/views/auth/passwords/mobile_number.blade.php
+++ resources/views/auth/passwords/mobile_number.blade.php
@@ -9,24 +9,24 @@
 
                 <div class="panel-body">
                     @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
+                    <div class="alert alert-success">
+                        {{ session('status') }}
+                    </div>
                     @endif
 
                     <form class="form-horizontal" method="POST" action="{{ route('password.email') }}">
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                            <label for="mobile_number" class="col-md-4 control-label">Mobile Number</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+                                <input id="mobile_number" type="mobile_number" class="form-control" name="mobile_number" value="{{ old('mobile_number') }}" required>
 
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
+                                @if ($errors->has('mobile_number'))
+                                <span class="help-block">
+                                    <strong>{{ $errors->first('mobile_number') }}</strong>
+                                </span>
                                 @endif
                             </div>
                         </div>

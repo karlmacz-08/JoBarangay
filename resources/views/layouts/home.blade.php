9ec226d620f4cca1e1c0679ba664f9bdc8e02821
Karl T. Macadangdang <karljarren0308@gmail.com>
2019-10-30 16:46:06 +0800

    Added bootstrap css and jquery links

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -3,6 +3,8 @@
 <head>
   <meta charset="UTF-8">
   <title>{{ config('app.name') }}</title>
+  <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+  <script src="{{ asset('js/jquery-3.4.1.min.js') }}"></script>
 @yield('resources')
 </head>
 <body>

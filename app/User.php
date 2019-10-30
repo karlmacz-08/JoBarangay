15f3a53c825ed16310a53d8192de8ade40f51474
Karl T. Macadangdang <karljarren0308@gmail.com>
2019-10-30 14:40:17 +0800

    Added Users-Skillsets relationship

--- app/User.php
+++ app/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
             return $this->first_name . ' ' . $this->last_name;
         }
     }
+
+    public function skills()
+    {
+      return $this->hasMany('App\Skillsets', 'user_id', 'id');
+    }
 }

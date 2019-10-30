a8c048a93764bda594bb80efd49db77033fd6631
Karl T. Macadangdang <karljarren0308@gmail.com>
2019-10-30 17:19:41 +0800

    New dashboard update(s)

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -5,10 +5,22 @@
   <title>{{ config('app.name') }}</title>
   <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
   <script src="{{ asset('js/jquery-3.4.1.min.js') }}"></script>
+  <script>
+    $(document).ready(function() {
+      $('body').on('click', '.logout-button', function() {
+        $('#logout-form').trigger('submit');
+
+        return false;
+      });
+    });
+  </script>
 @yield('resources')
 </head>
 <body>
 @yield('content')
 @yield('modals')
+  <form id="logout-form" action="{{ route('logout') }}" method="POST">
+    {{ csrf_field() }}
+  </form>
 </body>
 </html>
